ac43f4c4d1f950e470f83da29753d8844a9805b6
MattClement1 <matthew.clement1@ocsbstudent.ca>
2015-10-07 13:20:16 -0400

    Added Button

--- HelloWorld/HelloWorld.Designer.cs
+++ HelloWorld/HelloWorld.Designer.cs
@@ -29,23 +29,35 @@
         private void InitializeComponent()
         {
             this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.btnHelloWorld = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblHelloWorld
             // 
             this.lblHelloWorld.AutoSize = true;
             this.lblHelloWorld.Font = new System.Drawing.Font("Poor Richard", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHelloWorld.Location = new System.Drawing.Point(87, 40);
+            this.lblHelloWorld.Location = new System.Drawing.Point(82, 9);
             this.lblHelloWorld.Name = "lblHelloWorld";
             this.lblHelloWorld.Size = new System.Drawing.Size(111, 24);
             this.lblHelloWorld.TabIndex = 0;
             this.lblHelloWorld.Text = "Hello World!";
             // 
+            // btnHelloWorld
+            // 
+            this.btnHelloWorld.Location = new System.Drawing.Point(105, 60);
+            this.btnHelloWorld.Name = "btnHelloWorld";
+            this.btnHelloWorld.Size = new System.Drawing.Size(75, 23);
+            this.btnHelloWorld.TabIndex = 1;
+            this.btnHelloWorld.Text = "Hello World";
+            this.btnHelloWorld.UseVisualStyleBackColor = true;
+            this.btnHelloWorld.Click += new System.EventHandler(this.btnHelloWorld_Click);
+            // 
             // frmHelloWorld
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 105);
+            this.ClientSize = new System.Drawing.Size(284, 142);
+            this.Controls.Add(this.btnHelloWorld);
             this.Controls.Add(this.lblHelloWorld);
             this.Name = "frmHelloWorld";
             this.Text = "Hello World";
@@ -57,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.Button btnHelloWorld;
     }
 }
 
